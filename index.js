f8e8a299a052464dadeda7d5a4702317b802cc07
Mario Garcia <mag399@cornell.edu>
2019-06-01 15:08:17 -0700

    update flow to hide settings once set

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const { withUiHook, htm } = require('@zeit/integration-utils')
-var moment = require('moment');
-const mockData = require('./mockData');
-const issueView = require('./issueView');
-const data = mockData();
-require('isomorphic-fetch');
+const moment = require('moment');
 const { promisify } = require('util')
+require('isomorphic-fetch');
 
-const sleep = promisify(setTimeout)
 
-const store = {
-  secretId: '',
-  secretKey: ''
-};
+const issueView = require('./issueView');
+// const mockData = require('./mockData');
+// const data = mockData();
 
 const itemsPerPage = 10;
 let page = 1;
-
+let isMissingSettings = true;
+let showSettings = false;
 
 const getIssues = async (url, options = null) => {
   const response = await fetch(url, options);
   const json = await response.json();
   return Promise.resolve(json);
 }
-
 
 const throwDisplayableError = ({ message }) => {
   const error = new Error(message)
@@ -52,10 +47,8 @@ module.exports = withUiHook(async ({ payload, zeitClient }) => {
   if (!metadata.linkedApplications[projectId]) {
     metadata.linkedApplications[projectId] = {
       envAuthToken: '',
-
       organizationSlug: '',
       projectSlug: '',
-
       issues: [],
     }
   }
@@ -80,21 +73,25 @@ module.exports = withUiHook(async ({ payload, zeitClient }) => {
       await zeitClient.setMetadata(metadata)
       metadata.linkedApplications[projectId].projectSlug = clientState.projectSlug
       await zeitClient.setMetadata(metadata)
+
+      showSettings = false;
+    }
+
+    if (action == 'showSettings') {
+      showSettings = true;
     }
 
     if (action === 'getIssues') {
-      if(
+      if (
         !metadata.linkedApplications[projectId].envAuthToken ||
         !metadata.linkedApplications[projectId].organizationSlug ||
         !metadata.linkedApplications[projectId].projectSlug
-
-      ){
-        throwDisplayableError({ message: 'AUTH_TOKEN must be set' })
+      ) {
+        throwDisplayableError({ message: 'AUTH_TOKEN, ORGANIZATION_SLUG, and PROJECT_SLUG must be set.' })
       }
 
-      let issues_returned
+      let issues
       try {
-
         const options = {
           headers: {
             'Content-Type': 'application/json',
@@ -102,13 +99,11 @@ module.exports = withUiHook(async ({ payload, zeitClient }) => {
           },
         }
 
-        issues_returned = await getIssues(`https://sentry.io/api/0/projects/${metadata.linkedApplications[projectId].organizationSlug}/react/issues/`, options)
-        console.log(issues_returned)
+        issues = await getIssues(`https://sentry.io/api/0/projects/${metadata.linkedApplications[projectId].organizationSlug}/react/issues/`, options)
       } catch (err) {
-        console.error(err)
         throwDisplayableError({ message: 'There was an error fetching issues.' })
       }
-      metadata.linkedApplications[projectId].issues = issues_returned;
+      metadata.linkedApplications[projectId].issues = issues;
       await zeitClient.setMetadata(metadata)
     }
   } catch (err) {
@@ -132,46 +127,62 @@ module.exports = withUiHook(async ({ payload, zeitClient }) => {
   }
   const IssueView = issueView({ page, itemsPerPage, data: metadata.linkedApplications[projectId].issues })
 
-  console.log(metadata.linkedApplications[projectId].issues)
+  let isMissingSettings = (
+    !metadata.linkedApplications[projectId].envAuthToken ||
+    !metadata.linkedApplications[projectId].organizationSlug ||
+    !metadata.linkedApplications[projectId].projectSlug
+  );
 
   return htm`
-        <Page>
-            <Box marginBottom="10px" textAlign="right">
-              <ProjectSwitcher />
+    <Page>
+        ${(isMissingSettings || showSettings) ?
+          htm`
+            <Box>
+              <Box marginBottom="10px" textAlign="right">
+                <ProjectSwitcher />
+              </Box>
+              <Box display="flex" justifyContent="center" margin-bottom="2rem">
+                <Img src="https://sentry-brand.storage.googleapis.com/sentry-logo-black.png" />
+              </Box>
+              <Container>
+               ${errorMessage && htm`<Notice type="error">${errorMessage}</Notice>`}
+                <H1>Settings</H1>
+                <P>You can find your auth token at <Link href="https://sentry.io/settings/account/api/auth-tokens/" target="_blank">Sentry Auth Token</Link>. The configured keys will be availble as environment variables in your deployment as <B>AUTH_TOKEN</B> the next time you deploy.</P>
+                <Input
+                  label="AUTH_TOKEN"
+                  name="envAuthToken"
+                  value=${metadata.linkedApplications[projectId].envAuthToken}
+                  type="password"
+                  width="100%"
+                />
+               <Input
+                  label="ORGANIZATION_SLUG"
+                  name="organizationSlug"
+                  value=${metadata.linkedApplications[projectId].organizationSlug || ''}
+                  width="100%"
+                />
+               <Input
+                  label="PROJECT_SLUG"
+                  name="projectSlug"
+                  value=${metadata.linkedApplications[projectId].projectSlug || ''}
+                  width="100%"
+                />
+              </Container>
+              <Container>
+                <Button action="submit">Submit Keys</Button>
+              </Container>
             </Box>
-            <Box display="flex" justifyContent="center" margin-bottom="2rem">
-              <Img src="https://sentry-brand.storage.googleapis.com/sentry-logo-black.png" />
+          `:
+          htm`
+            <Box>
+              <Container>
+                <Button action="showSettings">Update Settings</Button>
+              </Container>
+              ${IssueView}
             </Box>
-            <Container>
-             ${errorMessage && htm`<Notice type="error">${errorMessage}</Notice>`}
-              <H1>Environment Variables</H1>
-              <P>These are the <Link href="https://docs.datadoghq.com/account_management/api-app-keys/" target="_blank">Sentry API and APP keys</Link>. The configured keys will be availble as environment variables in your deployment as <B>AUTH_TOKEN</B> the next time you deploy.</P>
-              <Input
-                label="AUTH_TOKEN"
-                name="envAuthToken"
-                value=${metadata.linkedApplications[projectId].envAuthToken}
-                type="password"
-                width="100%"
-              />
-             <Input
-                label="ORGANIZATION_SLUG"
-                name="organizationSlug"
-                value=${metadata.linkedApplications[projectId].organizationSlug || ''}
-                width="100%"
-              />
-             <Input
-                label="PROJECT_SLUG"
-                name="projectSlug"
-                value=${metadata.linkedApplications[projectId].projectSlug || ''}
-                width="100%"
-              />
-            </Container>
-            <Container>
-              <Button action="submit">Submit Keys</Button>
-            </Container>
-
-            ${IssueView}
-        </Page>
-    `
+          `
+        }
+    </Page>
+  `
 });
 
